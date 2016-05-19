dc317fb9aa2ccd554aaca661d9123b17b3de6915
Robert Hodgen <roberthodgen@gmail.com>
2016-05-19 07:28:46 -0400

    Untabify

--- src/countdown.js
+++ src/countdown.js
@@ -1,108 +1,108 @@
 (function () {
 
-	var app = angular.module('roberthodgen.countdown', []);
+  var app = angular.module('roberthodgen.countdown', []);
 
-	app.constant('COUNTDOWN_CLASSES', {
-		'normal': 'countdown-normal',
-		'warn': 'countdown-warn',
-		'danger': 'countdown-danger'
-	});
+  app.constant('COUNTDOWN_CLASSES', {
+    'normal': 'countdown-normal',
+    'warn': 'countdown-warn',
+    'danger': 'countdown-danger'
+  });
 
-	app.directive('countdown', [function () {
-		return {
-			restirct: 'E',
-			compile: function (element) {
-				element.addClass('countdown');
+  app.directive('countdown', [function () {
+    return {
+      restirct: 'E',
+      compile: function (element) {
+        element.addClass('countdown');
 
-				return { post: postlink };
+        return { post: postlink };
 
-				function postlink (scope, element, attrs, controller) {
-					controller.$element = element;
-					controller.endTimestamp = attrs.endTimestamp;
-					controller.start();
-				}
-			},
-			controller: ['$scope', '$interval', 'COUNTDOWN_CLASSES', function ($scope, $interval, COUNTDOWN_CLASSES) {
-				var self = this;
+        function postlink (scope, element, attrs, controller) {
+          controller.$element = element;
+          controller.endTimestamp = attrs.endTimestamp;
+          controller.start();
+        }
+      },
+      controller: ['$scope', '$interval', 'COUNTDOWN_CLASSES', function ($scope, $interval, COUNTDOWN_CLASSES) {
+        var self = this;
 
-				self.$element = null;
-				self.interval = null;
-				self.endTimestamp = null;
-				self.appliedClass = null;
+        self.$element = null;
+        self.interval = null;
+        self.endTimestamp = null;
+        self.appliedClass = null;
 
-				var warnSeconds = 10;
-				var dangerSeconds = 5;
+        var warnSeconds = 10;
+        var dangerSeconds = 5;
 
-				function Worker ($element, endTimestamp) {
-					var self = this;
-					self.$element = $element;
-					self.endTimestamp = endTimestamp;
-				}
+        function Worker ($element, endTimestamp) {
+          var self = this;
+          self.$element = $element;
+          self.endTimestamp = endTimestamp;
+        }
 
-				Worker.prototype.update = function () {
-					var self = this;
-					if (!angular.isDefined(self.endTimestamp) || self.endTimestamp === null) {
-						return;
-					}
+        Worker.prototype.update = function () {
+          var self = this;
+          if (!angular.isDefined(self.endTimestamp) || self.endTimestamp === null) {
+            return;
+          }
 
-					self.buildDates();
-					self.calculateTimeRemaining();
-					self.applyStyle();
-					self.writeToDom();
-				};
+          self.buildDates();
+          self.calculateTimeRemaining();
+          self.applyStyle();
+          self.writeToDom();
+        };
 
-				Worker.prototype.buildDates = function () {
-					var self = this;
-					self.endDate = new Date(self.endTimestamp);
-					self.nowDate = new Date();
-				};
+        Worker.prototype.buildDates = function () {
+          var self = this;
+          self.endDate = new Date(self.endTimestamp);
+          self.nowDate = new Date();
+        };
 
-				Worker.prototype.calculateTimeRemaining = function () {
-					var self = this,
-						seconds = self.endDate.getTime() - self.nowDate.getTime();
-					seconds = Math.floor(seconds / 1000);
-					self.seconds = Math.max(seconds, 0);
-				};
+        Worker.prototype.calculateTimeRemaining = function () {
+          var self = this,
+            seconds = self.endDate.getTime() - self.nowDate.getTime();
+          seconds = Math.floor(seconds / 1000);
+          self.seconds = Math.max(seconds, 0);
+        };
 
-				Worker.prototype.applyStyle = function () {
-					var self = this;
+        Worker.prototype.applyStyle = function () {
+          var self = this;
 
-					self.$element.removeClass(self.appliedClass);
+          self.$element.removeClass(self.appliedClass);
 
-					self.appliedClass = COUNTDOWN_CLASSES.normal;
+          self.appliedClass = COUNTDOWN_CLASSES.normal;
 
-					if (self.seconds <= warnSeconds) {
-						self.appliedClass = COUNTDOWN_CLASSES.warn;
-					}
+          if (self.seconds <= warnSeconds) {
+            self.appliedClass = COUNTDOWN_CLASSES.warn;
+          }
 
-					if (self.seconds <= dangerSeconds) {
-						self.appliedClass = COUNTDOWN_CLASSES.danger;
-					}
+          if (self.seconds <= dangerSeconds) {
+            self.appliedClass = COUNTDOWN_CLASSES.danger;
+          }
 
-					self.$element.addClass(self.appliedClass);
-				};
+          self.$element.addClass(self.appliedClass);
+        };
 
-				Worker.prototype.writeToDom = function () {
-					var self = this;
-					self.$element.text(self.seconds);
-				};
+        Worker.prototype.writeToDom = function () {
+          var self = this;
+          self.$element.text(self.seconds);
+        };
 
-				self.start = function () {
-					self.interval = $interval(self.update, 1 * 1000, self);
-					self.update();
-				};
+        self.start = function () {
+          self.interval = $interval(self.update, 1 * 1000, self);
+          self.update();
+        };
 
-				self.update = function () {
-					var time = $scope.$eval(self.endTimestamp);
-					new Worker(self.$element, time).update();
-				};
+        self.update = function () {
+          var time = $scope.$eval(self.endTimestamp);
+          new Worker(self.$element, time).update();
+        };
 
-				$scope.$on('$destroy', function () {
-					$interval.cancel(self.interval);
-					self.$element = null;
-				});
-			}]
-		};
-	}]);
+        $scope.$on('$destroy', function () {
+          $interval.cancel(self.interval);
+          self.$element = null;
+        });
+      }]
+    };
+  }]);
 
 })();
